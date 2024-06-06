01422087eebbc3b27e8cc9a59b3e22fbdc18187e
Félix Piédallu <felix@piedallu.me>
2024-06-06 12:02:24 +0200

    Automated packaging v2 conversion

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="acl python3 python3-pip python3-venv nodejs postgresql postgresql-contrib postgresql-common redis-server libsasl2-dev libldap2-dev libssl-dev build-essential curl gfortran gnupg libatlas-base-dev libblas-dev libblas3 libfreetype6 libfreetype6-dev libhdf5-dev libheif-examples libjpeg-dev liblapack-dev liblapack3 libpq-dev libtiff5-dev netcat libimage-exiftool-perl"
+#REMOVEME? pkg_dependencies="acl python3 python3-pip python3-venv nodejs postgresql postgresql-contrib postgresql-common redis-server libsasl2-dev libldap2-dev libssl-dev build-essential curl gfortran gnupg libatlas-base-dev libblas-dev libblas3 libfreetype6 libfreetype6-dev libhdf5-dev libheif-examples libjpeg-dev liblapack-dev liblapack3 libpq-dev libtiff5-dev netcat libimage-exiftool-perl"
 
 #=================================================
 # PERSONAL HELPERS
@@ -16,11 +16,11 @@ pkg_dependencies="acl python3 python3-pip python3-venv nodejs postgresql postgre
 #=================================================
 
 function set_permissions {
-	mkdir -p "$final_path/srv"
-	chown -R root:$app "$final_path"
-	chmod -R g=u,g-w,o-rwx "$final_path"
-	setfacl -n -m g:www-data:--x "$final_path";
-	setfacl -nR -m d:g:www-data:r-x -m g:www-data:r-x "$final_path/srv";
+	mkdir -p "$install_dir/srv"
+	chown -R root:$app "$install_dir"
+	chmod -R g=u,g-w,o-rwx "$install_dir"
+	setfacl -n -m g:www-data:--x "$install_dir";
+	setfacl -nR -m d:g:www-data:r-x -m g:www-data:r-x "$install_dir/srv";
 
 	mkdir -p "$data_path/data/"{photos,raw-photos-processed,cache,models}
 	chown -R $app:$app "$data_path"
@@ -34,8 +34,8 @@ function set_permissions {
 }
 
 function patch_files {
-	find "$final_path" -type f | while read file; do
-		ynh_replace_string -m "'/srv" -r "'$final_path/srv" -f "$file"
+	find "$install_dir" -type f | while read file; do
+		ynh_replace_string -m "'/srv" -r "'$install_dir/srv" -f "$file"
 		ynh_replace_string -m "'/data" -r "'$data_path/data" -f "$file"
 	done
 
@@ -43,21 +43,21 @@ function patch_files {
 }
 
 function build_django_backend {
-	chown -R $app:$app "$final_path"
-	pushd "$final_path"
-		sudo -u $app python3 -m venv "$final_path/venv"
-		sudo -u $app $final_path/venv/bin/pip --cache-dir "$final_path/.cache/pip" install -U wheel pip setuptools 2>&1
+	chown -R $app:$app "$install_dir"
+	pushd "$install_dir"
+		sudo -u $app python3 -m venv "$install_dir/venv"
+		sudo -u $app $install_dir/venv/bin/pip --cache-dir "$install_dir/.cache/pip" install -U wheel pip setuptools 2>&1
 		while read requirement ; do
 			if [ ! -z "$requirement" ] && [[ ! "$requirement" =~ ^'#'.*  ]]; then
-				sudo -u $app PYTHONUNBUFFERED=1 "$final_path/venv/bin/pip" --cache-dir "$final_path/.cache/pip" install "$requirement" 2>&1
+				sudo -u $app PYTHONUNBUFFERED=1 "$install_dir/venv/bin/pip" --cache-dir "$install_dir/.cache/pip" install "$requirement" 2>&1
 			fi
-		done < "$final_path/requirements.txt"
-		sudo -u $app $final_path/venv/bin/pip --cache-dir "$final_path/.cache/pip" install -U django-auth-ldap 2>&1
-		sudo -u $app mkdir -p "$final_path/srv"
-		sudo -u $app cp -rT "$final_path/photonix" "$final_path/srv/photonix"
+		done < "$install_dir/requirements.txt"
+		sudo -u $app $install_dir/venv/bin/pip --cache-dir "$install_dir/.cache/pip" install -U django-auth-ldap 2>&1
+		sudo -u $app mkdir -p "$install_dir/srv"
+		sudo -u $app cp -rT "$install_dir/photonix" "$install_dir/srv/photonix"
 	popd
-	pushd "$final_path/srv"
-		sudo -u $app $final_path/venv/bin/python "$final_path/srv/photonix/manage.py" collectstatic --noinput --link 2>&1
+	pushd "$install_dir/srv"
+		sudo -u $app $install_dir/venv/bin/python "$install_dir/srv/photonix/manage.py" collectstatic --noinput --link 2>&1
 	popd
 
 	set_permissions
@@ -72,47 +72,47 @@ function set_node_vars {
 function build_node_frontend {
 	set_node_vars
 
-	chown -R $app:$app "$final_path"
-	sudo -u $app touch "$final_path/.yarnrc"
-	sudo -u $app mkdir -p "$final_path/srv/ui"
-	pushd "$final_path"
-		sudo -u $app cp -T "$final_path/ui/package.json" "$final_path/srv/ui/package.json"
-		sudo -u $app cp -T "$final_path/ui/yarn.lock" "$final_path/srv/ui/yarn.lock"
+	chown -R $app:$app "$install_dir"
+	sudo -u $app touch "$install_dir/.yarnrc"
+	sudo -u $app mkdir -p "$install_dir/srv/ui"
+	pushd "$install_dir"
+		sudo -u $app cp -T "$install_dir/ui/package.json" "$install_dir/srv/ui/package.json"
+		sudo -u $app cp -T "$install_dir/ui/yarn.lock" "$install_dir/srv/ui/yarn.lock"
 	popd
 
-	pushd "$final_path/srv/ui"
-		sudo -u $app env "PATH=$node_path" yarn --cache-folder $final_path/yarn-cache --use-yarnrc $final_path/.yarnrc install 2>&1
-		sudo -u $app cp -rT "$final_path/ui/public" "$final_path/srv/ui/public"
-		sudo -u $app cp -rT "$final_path/ui/src" "$final_path/srv/ui/src"
-		sudo -u $app env "PATH=$node_path" yarn --cache-folder $final_path/yarn-cache --use-yarnrc $final_path/.yarnrc build 2>&1
+	pushd "$install_dir/srv/ui"
+		sudo -u $app env "PATH=$node_path" yarn --cache-folder $install_dir/yarn-cache --use-yarnrc $install_dir/.yarnrc install 2>&1
+		sudo -u $app cp -rT "$install_dir/ui/public" "$install_dir/srv/ui/public"
+		sudo -u $app cp -rT "$install_dir/ui/src" "$install_dir/srv/ui/src"
+		sudo -u $app env "PATH=$node_path" yarn --cache-folder $install_dir/yarn-cache --use-yarnrc $install_dir/.yarnrc build 2>&1
 	popd
 
-	sudo -u $app cp -rT "$final_path/ui/public" "$final_path/srv/ui/public"
+	sudo -u $app cp -rT "$install_dir/ui/public" "$install_dir/srv/ui/public"
 
 	set_permissions
 }
 
 function add_envfile {
-	secret_key=$(ynh_app_setting_get --app=$app --key=secret_key)
+#REMOVEME? 	secret_key=$(ynh_app_setting_get --app=$app --key=secret_key)
 
 	if [ -z $secret_key ]; then
 		secret_key=$(ynh_string_random -l 64)
 		ynh_app_setting_set --app=$app --key=secret_key --value=$secret_key
 	fi
 
-	ynh_add_config --template="photonix.env" --destination="$final_path/photonix.env"
+	ynh_add_config --template="photonix.env" --destination="$install_dir/photonix.env"
 
 	set_permissions
 }
 
 function apply_db_migrations {
-	pushd "$final_path/srv/photonix" || ynh_die
+	pushd "$install_dir/srv/photonix" || ynh_die
 		sudo -u $app bash -c "
-			source \"$final_path/venv/bin/activate\"
+			source \"$install_dir/venv/bin/activate\"
 			set -a
-			source \"$final_path/photonix.env\"
-			python \"$final_path/photonix/manage.py\" makemigrations
-			python \"$final_path/photonix/manage.py\" migrate"
+			source \"$install_dir/photonix.env\"
+			python \"$install_dir/photonix/manage.py\" makemigrations
+			python \"$install_dir/photonix/manage.py\" migrate"
 	popd
 }
 
@@ -173,20 +173,20 @@ function backup_services {
 }
 
 function load_settings {
-	final_path="$(ynh_app_setting_get --app=$app --key=final_path)"
-	data_path="$(ynh_app_setting_get --app=$app --key=data_path)"
-	domain=$(ynh_app_setting_get --app=$app --key=domain)
-	path_url=$(ynh_app_setting_get --app=$app --key=path)
-	db_name=$(ynh_app_setting_get --app=$app --key=db_name)
+#REMOVEME? #REMOVEME? 	install_dir="$(ynh_app_setting_get --app=$app --key=install_dir)"
+#REMOVEME? 	data_path="$(ynh_app_setting_get --app=$app --key=data_path)"
+#REMOVEME? 	domain=$(ynh_app_setting_get --app=$app --key=domain)
+#REMOVEME? 	path=$(ynh_app_setting_get --app=$app --key=path)
+#REMOVEME? 	db_name=$(ynh_app_setting_get --app=$app --key=db_name)
 	db_user=$db_name
-	db_pwd=$(ynh_app_setting_get --app=$app --key=psqlpwd)
-	admin=$(ynh_app_setting_get --app=$app --key=admin)
-	port=$(ynh_app_setting_get --app=$app --key=port)
-	classification_color_enabled=$(ynh_app_setting_get --app=$app --key=classification_color_enabled)
-	classification_location_enabled=$(ynh_app_setting_get --app=$app --key=classification_location_enabled)
-	classification_face_enabled=$(ynh_app_setting_get --app=$app --key=classification_face_enabled)
-	classification_style_enabled=$(ynh_app_setting_get --app=$app --key=classification_style_enabled)
-	classification_object_enabled=$(ynh_app_setting_get --app=$app --key=classification_object_enabled)
+#REMOVEME? 	db_pwd=$(ynh_app_setting_get --app=$app --key=psqlpwd)
+#REMOVEME? 	admin=$(ynh_app_setting_get --app=$app --key=admin)
+#REMOVEME? 	port=$(ynh_app_setting_get --app=$app --key=port)
+#REMOVEME? 	classification_color_enabled=$(ynh_app_setting_get --app=$app --key=classification_color_enabled)
+#REMOVEME? 	classification_location_enabled=$(ynh_app_setting_get --app=$app --key=classification_location_enabled)
+#REMOVEME? 	classification_face_enabled=$(ynh_app_setting_get --app=$app --key=classification_face_enabled)
+#REMOVEME? 	classification_style_enabled=$(ynh_app_setting_get --app=$app --key=classification_style_enabled)
+#REMOVEME? 	classification_object_enabled=$(ynh_app_setting_get --app=$app --key=classification_object_enabled)
 }
 
 #=================================================
